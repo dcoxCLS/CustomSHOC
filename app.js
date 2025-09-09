dc5390b349afbbe7077380cf03964a041a9bd92e
dcoxCLS <coxda@cofc.edu>
2025-09-09 16:48:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  const opacityInput = document.getElementById('sliderDiv');
+const opacityInput = document.getElementById('sliderDiv');
 
 			const FeatureLayer = await $arcgis.import("@arcgis/core/layers/FeatureLayer.js");
             const TileLayer = await $arcgis.import("@arcgis/core/layers/TileLayer.js");
@@ -11,83 +11,165 @@
 			const defaultFilter = document.querySelector("#defaultFilter");
 			let whereClause = defaultOption.value;
 			const zoom = viewElement.zoom;
+			const featureNode = document.querySelector("#feature-node");
+			let highlight;
+          	let objectId;
 
 			viewElement.addEventListener("arcgisViewReadyChange", () => {
     // All layer, popup, and event listener declarations should go here
 
     // Define popup for points layer
     const popupPoints = {
-        title: "{RESNAME}",
+        title: "{orig_address_no} {orig_address_street}",
         content:
-            "Street Address: {Address}<br>Municipality: {City}<br>URL: <a href={NARA_URL}>View URL</a>",
+            "Original Street Address: {orig_address_no} {orig_address_street}<br>Municipality: {orig_city}<br>Primary Material: {prime_material}<br>Primary Function: {function_prime}<br>Place Description: {place_descript}<br>Source: 1902 Sanborn",
     };
     
-const points = {
+    const points = {
     type: "unique-value",
-    field: "Is_NHL",
-    field2: "STATUS",
-    fieldDelimiter: ",",
+    field: "prime_material",
     uniqueValueInfos: [{
-        value: "X,Listed",
+        value: "wood frame",
         symbol: {
             type: "simple-marker", // Added type
             style: "circle",
-            color: [0, 112, 255, 255],
-            size: 10.0,
-            angle: 0.0,
-            xoffset: 0,
-            yoffset: 0,
-            outline: { // Corrected outline object
-                color: [0, 112, 255, 255],
-                width: 1
-            }
-        }
-    },
-    {
-        value: "<Null>,Listed",
-        symbol: {
-            type: "simple-marker", // Added type
-            style: "circle",
-            color: [255, 170, 0, 255],
-            size: 10.0,
-            angle: 0.0,
-            xoffset: 0,
-            yoffset: 0,
-            outline: { // Corrected outline object
-                color: [255, 170, 0, 255],
-                width: 1
-            }
-        }
-    },
-    {
-        value: "<Null>,Removed", // Added a new unique value for "Removed"
-        symbol: {
-            type: "simple-marker",
-            style: "circle",
-            color: "gray",
-            size: 10.0,
+            color: "#660000",
+            size: 5.0,
             angle: 0.0,
             xoffset: 0,
             yoffset: 0,
             outline: {
-                color: "gray",
+                color: "#660000",
+                width: 1
+            }
+        }
+    },
+        {
+        value: "brick", 
+        symbol: {
+            type: "simple-marker",
+            style: "circle",
+            color: "#bfa87c",
+            size: 5.0,
+            angle: 0.0,
+            xoffset: 0,
+            yoffset: 0,
+            outline: {
+                color: "#bfa87c",
                 width: 1
             }
         }
     }]
 };
     
+//const points = {
+    //type: "unique-value",
+    //field: "Is_NHL",
+    //field2: "STATUS",
+    //fieldDelimiter: ",",
+    //uniqueValueInfos: [{
+        //value: "X,Listed",
+        //symbol: {
+            //type: "simple-marker", // Added type
+            //style: "circle",
+            //color: [0, 112, 255, 255],
+            //size: 10.0,
+            //angle: 0.0,
+            //xoffset: 0,
+            //yoffset: 0,
+            //outline: { // Corrected outline object
+                //color: [0, 112, 255, 255],
+                //width: 1
+            //}
+        //}
+    //},
+    //{
+        //value: "<Null>,Listed",
+        //symbol: {
+            //type: "simple-marker", // Added type
+            //style: "circle",
+            //color: [255, 170, 0, 255],
+            //size: 10.0,
+            //angle: 0.0,
+            //xoffset: 0,
+            //yoffset: 0,
+            //outline: { // Corrected outline object
+                //color: [255, 170, 0, 255],
+                //width: 1
+            //}
+        //}
+    //},
+    //{
+        //value: "<Null>,Removed", // Added a new unique value for "Removed"
+        //symbol: {
+            //type: "simple-marker",
+            //style: "circle",
+            //color: "gray",
+            //angle: 0.0,
+            //xoffset: 0,
+            //yoffset: 0,
+            //outline: {
+                //color: "gray",
+                //width: 1
+            //}
+        //}
+    //}]
+//};
+    
     // Define points layer
+    //const pointsLayer = new FeatureLayer({
+        //url: "https://mapservices.nps.gov/arcgis/rest/services/cultural_resources/nrhp_locations/MapServer/0",
+        //outFields: ["RESNAME", "Address", "City", "NARA_URL", "Is_NHL", "STATUS"],
+        //popupTemplate: popupPoints,
+        //renderer: points,
+    //});
+    
     const pointsLayer = new FeatureLayer({
-        url: "https://mapservices.nps.gov/arcgis/rest/services/cultural_resources/nrhp_locations/MapServer/0",
-        outFields: ["RESNAME", "Address", "City", "NARA_URL", "Is_NHL", "STATUS"],
-        popupTemplate: popupPoints,
+        url: "https://lyre.cofc.edu/server/rest/services/shoc/pl_sanborn1902/FeatureServer/0",
+        outFields: ["prime_material", "function_prime", "place_descript", "orig_address_no", "orig_address_street", "orig_city"],
+        //popupTemplate: popupPoints,
         renderer: points,
     });
     
     viewElement.map.add(pointsLayer, 1);
 
     // All event listeners are now consolidated in one place
+    
+viewElement.view.on("click", (event) => {
+        viewElement.view.hitTest(event).then(function(response) {
+    	if (response.results.length > 0) {
+    const graphic = response.results[0].graphic;
+    const prefix = graphic.attributes;
+    
+    // Check if the prefix object is defined before trying to access its properties
+    if (prefix.orig_city !== undefined) {
+        const contentHTML = `
+        <h2>${prefix.orig_address_no} ${prefix.orig_address_street}</h2>
+            <b>Original Street Address:</b> ${prefix.orig_address_no} ${prefix.orig_address_street}<br>
+            <b>Municipality:</b> ${prefix.orig_city}<br>
+            <b>Primary Material:</b> ${prefix.prime_material}<br>
+            <b>Primary Function:</b> ${prefix.function_prime}<br>
+            <b>Place Description:</b> ${prefix.place_descript}<br>
+            <b>Source:</b> 1902 Sanborn
+        `;
+        pointsInfo.innerHTML = contentHTML;
+        featureNode.style.display = "block";
+        featureNode.style.width= "25vw";
+        viewElement.style.width="75vw";
+        
+    } else {
+        pointsInfo.innerHTML = "No points selected";
+        featureNode.style.display = "none";
+        viewElement.style.width="100vw";
+    }
+} else {
+    pointsInfo.innerHTML = "No points selected";
+    featureNode.style.display = "none";
+    viewElement.style.width="100vw";
+}
+    });
+});
+	
     viewElement.addEventListener("arcgisViewChange", (event) => {
         queryCount(viewElement.extent);
     });
@@ -116,14 +198,11 @@ pointsFilterMenu.addEventListener("change", (event) => {
     let pointsFilterExpression = "";
 
     switch (selectedValue) {
-        case "NHL":
-            pointsFilterExpression = "Is_NHL IS NOT NULL AND STATUS = 'Listed'";
+        case "brick":
+            pointsFilterExpression = "prime_material = 'brick'";
             break;
-        case "NRHP":
-            pointsFilterExpression = "Is_NHL IS NULL AND STATUS = 'Listed'";
-            break;
-        case "removed":
-            pointsFilterExpression = "STATUS = 'Removed'";
+        case "wood":
+            pointsFilterExpression = "prime_material = 'wood frame'";
             break;
         case "both":
         default:
@@ -209,6 +288,7 @@ function queryCount(extent) {
     if (whereClause === '1=0') {
         if (tileLayer) {
             viewElement.map.remove(tileLayer);
+            mapsInfo.innerHTML = "No maps selected"
         }
         // This line is crucial to ensure the historic map is not loaded
         viewElement.graphics.removeAll();
@@ -238,6 +318,7 @@ let tileLayer;
 
 function displayResults(results) {
     const service_url = results.features[0]?.attributes?.service_url;
+    const mapPrefix = results.features[0]?.attributes;
 
     if (!service_url) {
         console.error("No service_url found in feature attributes.");
@@ -247,6 +328,7 @@ function displayResults(results) {
     // Remove previous tile layer if it exists
     if (tileLayer) {
         viewElement.map.remove(tileLayer);
+        mapsInfo.innerHTML = "No maps selected"
     }
     
     // Remove the points layer before adding the new tile layer, so we can re-add it on top
@@ -264,12 +346,26 @@ function displayResults(results) {
 
     // Add the tile layer first, at the bottom of the stack
     viewElement.map.add(tileLayer, 0);
+    
+    
+    featureNode.style.display = "block";
+    featureNode.style.width= "25vw";
+    viewElement.style.width="75vw";
+    mapsInfo.innerHTML = `<h2>${mapPrefix.mapyear} ${mapPrefix.title}</h2>
+                <b>Title:</b> ${mapPrefix.title}<br>
+            <b>Date:</b> ${mapPrefix.mapmonth}/${mapPrefix.mapday}/${mapPrefix.mapyear}<br>
+            <b>Description:</b> ${mapPrefix.source_description}<br>
+            <b>Publisher:</b> ${mapPrefix.publisher}<br>
+            <b>Author:</b> ${mapPrefix.author}<br>
+            <b>Cartographer/Surveyor:</b> ${mapPrefix.cartographer_surveyor}<br>
+            <b>Original Repository:</b> ${mapPrefix.orig_repository}<br>
+            <a href=${mapPrefix.service_url}>View Service URL</a>`;
 
     // Re-add the points layer on top of the tile layer
     const pointsLayer = new FeatureLayer({
         url: "https://mapservices.nps.gov/arcgis/rest/services/cultural_resources/nrhp_locations/MapServer/0",
         outFields: ["RESNAME", "Address", "City", "NARA_URL", "Is_NHL", "STATUS"],
-        popupTemplate: popupPoints,
+        //popupTemplate: popupPoints,
         renderer: points,
         id: "pointsLayer" // Give the layer an ID for easy referencing
     });
@@ -292,7 +388,7 @@ function displayResults(results) {
 
     results.features.map((feature) => {
         feature.symbol = symbol;
-        feature.popupTemplate = popupTemplate;
+        //feature.popupTemplate = popupTemplate;
         return feature;
     });
 
@@ -310,5 +406,7 @@ opacityInput.addEventListener('mouseup', function() {
         console.log("Range Slider has value of " + this.value);
     }
 });
+
+
   const rangeInput = document.getElementById('sliderDiv');
   const rangeOutput = document.getElementById('rangeValue');
